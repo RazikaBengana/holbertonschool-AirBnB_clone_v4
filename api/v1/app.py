9365b5266de291b58320f4090c0284498988fdbb
RazikaBengana <razikabengana@gmail.com>
2024-04-18 22:45:52 +0200

    Update `CORS` settings in `api/v1/app.py` that was restricted to requests from `0.0.0.0` to allow now requests from all origins, broadening API accessibility

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,31 +1,49 @@
 #!/usr/bin/python3
-"""This is a Flask application setup script for a RESTful API server"""
+"""This script sets up a Flask application serving a Restful API for the AirBnB clone"""
 
-from api.v1.views import app_views
-from flask import Flask, make_response, jsonify
+from flask import Flask, render_template, make_response, jsonify
 from models import storage
-from os import getenv
+from api.v1.views import app_views
+from os import environ
 from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
-CORS(app, origins="0.0.0.0")  # Enable CORS for all domains
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
-def teardown_appcont(exception):
+def close_db(error):
     """Close the database session at the end of the request or when the application shuts down"""
     storage.close()
 
+
 @app.errorhandler(404)
-def page_not_found(exception):
-    """Return a custom 404 JSON response when a page is not found"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+def not_found(error):
+    """Return a custom JSON error response for 404 not found errors"""
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
 
 
 if __name__ == "__main__":
-    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=int(getenv("HBNB_API_PORT", "5000")),
-            threaded=True, debug=True)
+    """Run the Flask application on host and port specified in the environment variables or default settings"""
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+
+    app.run(host=host, port=port, threaded=True)
